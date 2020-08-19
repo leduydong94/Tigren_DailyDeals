543c064b140905842e5147385c1ee6e41ef6bdc2
leduydong94 <leduydong281@gmail.com>
2020-08-19 15:39:01 +0700

    update status

--- app/code/Tigren/DailyDeals/Ui/Component/Listing/Column/Status.php
+++ app/code/Tigren/DailyDeals/Ui/Component/Listing/Column/Status.php
@@ -11,6 +11,6 @@ class Status implements \Magento\Framework\Option\ArrayInterface
      */
     public function toOptionArray()
     {
-        return [['value' => 1, 'label' => __('Enable')], ['value' => 0, 'label' => __('Disable')]];
+        return [['value' => 1, 'label' => __('Enable')], ['value' => 0, 'label' => __('Disable')], ['value' => 2, 'label' => __('Ended')]];
     }
 }
